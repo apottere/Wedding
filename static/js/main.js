c8ac27d2e0fa05d514061757a95c6e71031de1ec
Andrew Potter <apottere@gmail.com>
2014-12-07 02:57:11 -0500

    Add navigation and sumbodule.

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,16 @@
-angular.module('app', []).controller('WeddingController', function($scope) {
+angular.module('app', []).config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode({enabled: true, requireBase: false}).hashPrefix('!');
+
+}]).controller('WeddingController', ['$anchorScroll', '$location', '$scope', function($anchorScroll, $location, $scope) {
     $scope.mobileMenu = false;
 
     $scope.menuClick = function() {
         $scope.mobileMenu = !$scope.mobileMenu;
-    }
-});
+    };
+
+    $scope.setHash = function(target) {
+        $scope.mobileMenu = false;
+        $location.hash(target);
+        $anchorScroll();
+    };
+}]);
